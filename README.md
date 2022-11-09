2a063ecf5c57ddea161e0d068ce8fa9bbc089c05
DAYEON CHOI <dayeon2399@naver.com>
2022-11-09 15:56:22 +0900

    선택 완료 버튼 및 레이아웃 수정 (#1)

--- README.md
+++ README.md
@@ -1,5 +1,18 @@
 # springboot-ui-kit
 
+----
+
+<br />
+
+### 활용 라이브러리
+
+---
+* [Date Range Picker - JavaScript Date & Time Picker Library](https://www.daterangepicker.com/)
+
+
 ### 참고 문서 링크
+
+---
+
 * [layout, fragment 활용 구조 참고](https://dev-jwblog.tistory.com/34)
 * [layout 활용 구조 참고](https://attacomsian.com/blog/thymeleaf-fragments)
--- src/main/resources/static/js.app/calendar.js
+++ src/main/resources/static/js.app/calendar.js
@@ -5,19 +5,34 @@ var calendar = {
         _this.makeUpDatepicker();
     },
 
+    /* 날짜 범위 선택기 */
     makeUpDatepicker: function() {
-        let picker = $('#datepicker').daterangepicker({
+        // 초기화
+        $('#date-range-picker')
+            .val(calendar.getDateNow() + " ~ " + calendar.getDateNow())
+            .on('click', function() {
+                $('.daterangepicker').show();
+            });
+
+        // 기본 옵션 사용
+        let picker = $('#date-range-picker').daterangepicker({
                     locale: {
                         "separator": " ~ ",
                         "format": "YYYY.MM.DD",
                         "daysOfWeek": ["<span style='color: #EA4E50;'>일</span>", "월", "화", "수", "목", "금", "<span style='color: #004FC5;'>토</span>"],
                         "monthNames": ["1월", "2월", "3월", "4월", "5월", "6월", "7월", "8월", "9월", "10월", "11월", "12월"],
-                        "applyLabel": "선택 완료",
+                        "applyLabel": " 선택 완료",
                     },
                     opens: "center",
-                    autoApply: true,
                     linkedCalendars: false,
+                    autoUpdateInput: false,
                 });
+
+        // input 옆 날짜 계산 표시
+        picker.on('change', function() {
+            calendar.setPickedTotal();
+        });
+        calendar.setPickedTotal();
 
         // 기존 캘린더 2달치 표시 -> 1달만 표시
         $('.drp-calendar.right').hide();
@@ -29,18 +44,39 @@ var calendar = {
             el.append('<span></span>');
         });
 
-        calendar.setPickedTotal();
-
-        picker.on('change', function() {
+        // 선택 완료 버튼
+        $('.drp-calendar.left').append('<button type="button" id="apply-button" class="apply-button applyBtn"></button>');
+        $('.drp-selected').on('DOMSubtreeModified', function() {
+            calendar.setApplyButtonText();
+        });
+        $('#apply-button').on('click', function() {
+            calendar.onApplyButtonClick();
             calendar.setPickedTotal();
         });
     },
 
+    onApplyButtonClick: function() {
+        $('#date-range-picker').val($('.drp-selected').text());
+        $('.daterangepicker').hide();
+    },
+
     setPickedTotal: function() {
-        const startToEndArray = $('#datepicker').val().split(" ");
+        const startToEndArray = $('#date-range-picker').val().split(" ");
         const diffText = calendar.getDateDiffText(startToEndArray[0], startToEndArray[2]);
-        $('#pickedTotal').html("");
-        $('#pickedTotal').append(diffText);
+        $('#picked-total').html("").append(diffText);
+    },
+
+    setApplyButtonText: function() {
+        const startToEndArray = $('.drp-selected').text().split(" ");
+        const diffText = calendar.getDateDiffText(startToEndArray[0], startToEndArray[2]);
+        $('#apply-button').html("").append(diffText+" 선택 완료");
+        return diffText;
+    },
+
+    getDatePickerDiffText: function() {
+        const startToEndArray = $('#date-range-picker').val().split(" ");
+        const diffText = calendar.getDateDiffText(startToEndArray[0], startToEndArray[2]);
+        return diffText;
     },
 
     getDateDiffText: function(start, end) {
@@ -51,8 +87,13 @@ var calendar = {
     getDateDiff: function(start, end) {
         const startDate = new Date(start);
         const endDate = new Date(end);
-
         return Math.abs((endDate.getTime() - startDate.getTime()) / (1000 * 3600 * 24));
+    },
+
+    getDateNow: function() {
+        const date = new Date();
+        const dateText = date.getFullYear() + "." + (date.getMonth()+1) + "." + ('00'+date.getDate()).slice(-2);
+        return dateText;
     },
 };
 
